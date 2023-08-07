2f608978428af053759c95647c427567d8996b86
rafaelbr555 <rafaelcongarattodev@gmail.com>
2023-08-07 17:48:17 -0300

    alterações

--- challenge/main.js
+++ challenge/main.js
@@ -1,7 +1,22 @@
-const darkmode = window.document.querySelector('.dark-mode')
+/*const darkmode = window.document.querySelector('.dark-mode')
+const começar = window.document.querySelector('.começar')
 
+
+const iniciarCronometro = () =>{
+    let tempo = 25
+    for(c in tempo){
+        console.log(tempo)
+    }
+}
+
+começar.addEventListener('click', iniciarCronometro)
 
 darkmode.addEventListener('click', function(){
 
     document.body.classList.toggle('dark')
 })
+*/
+var A = 10;
+var B = 5;
+var X = Number(A) + Number(B);
+console.log(`X = ${X}`);
