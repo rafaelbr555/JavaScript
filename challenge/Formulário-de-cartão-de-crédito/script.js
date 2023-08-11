abb80a1f4f6c733b2036896ee04cd2e90666d189
rafaelbr555 <rafaelcongarattodev@gmail.com>
2023-08-10 23:44:39 -0300

    Validação da quantidade de numeros do campo inputNumber

--- challenge/Formulário-de-cartão-de-crédito/script.js
+++ challenge/Formulário-de-cartão-de-crédito/script.js
@@ -35,7 +35,7 @@ function substituirCvc(e) {
 }
 
 function verificarError(e) {
-  e.preventDefault();
+  e.preventDefault();  
   if (!inputName.value) {
     inputName.classList.add("error");
     inputName.parentElement.classList.add("error_mensagem");
@@ -43,12 +43,35 @@ function verificarError(e) {
     inputName.classList.remove("error");
     inputName.parentElement.classList.remove("error_mensagem");
   }
-  if (!inputNumber.value) {
-    inputNumber.classList.add("error");
-    inputNumber.parentElement.classList.add("error_mensagem");
+  if(!inputNumber.value){
+    inputNumber.classList.add('error')
+    inputNumber.parentElement.classList.add('error_mensagem')
+  }else{
+    inputNumber.classList.remove('error_mensagem')
+    inputNumber.parentElement.classList.remove('error_mensagem')
+  }
+  if (inputNumber.value.length >=1 && inputNumber.value.length < 16) {
+    inputNumber.classList.add('incomplete')
+    inputNumber.parentElement.classList.add("error_incomplete");
   } else {
-    inputNumber.classList.remove("error");
-    inputNumber.parentElement.classList.remove("error_mensagem");
+    inputNumber.classList.remove('incomplete')
+    inputNumber.parentElement.classList.remove("error_incomplete");
+  }
+  if (!inputMes.value || !inputAno.value) {
+    inputAno.classList.add("error");
+    inputMes.classList.add("error");
+    inputMes.parentElement.classList.add("error_mensagem");
+  } else {
+    inputAno.classList.remove("error");
+    inputMes.classList.remove("error");
+    inputMes.parentElement.classList.remove("error_mensagem");
+  }
+  if (!inputCvc.value) {
+    inputCvc.classList.add("error");
+    inputCvc.parentElement.classList.add("error_mensagem");
+  } else {
+    inputCvc.classList.remove("error");
+    inputCvc.parentElement.classList.remove("error_mensagem");
   }
 }
 
